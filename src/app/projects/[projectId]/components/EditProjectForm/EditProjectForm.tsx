60fadd4aa322bca161807d364cacbc43e318a0ce
mbitirini <mariabitirini@gmail.com>
2024-07-12 11:26:09 +0300

    Create an auto-save feature

--- src/app/projects/[projectId]/components/EditProjectForm/EditProjectForm.tsx
+++ src/app/projects/[projectId]/components/EditProjectForm/EditProjectForm.tsx
@@ -1,28 +1,17 @@
 'use client'
 
 import { updateProject } from '@/app/projects/[projectId]/actions/update-project'
-import { LoadingButton } from '@mui/lab'
-import {
-  FormControl,
-  Grid,
-  Input,
-  InputLabel,
-  Paper,
-  Stack,
-  TextField,
-  Typography,
-} from '@mui/material'
+import { Grid, Paper, TextField, Typography } from '@mui/material'
 import { Project } from '@prisma/client'
 import { useState } from 'react'
+import useDebounce from '@/app/projects/hooks/useDebounce'
 
 export const EditProjectForm = ({ project }: { project: Project }) => {
   const [title, setTitle] = useState(project.title)
   const [subhead, setSubhead] = useState(project.subhead)
   const [description, setDescription] = useState(project.description)
-  const [isLoading, setIsLoading] = useState(false)
 
   const onSave = async () => {
-    setIsLoading(true)
     try {
       await updateProject({
         id: project.id,
@@ -35,9 +24,30 @@ export const EditProjectForm = ({ project }: { project: Project }) => {
         project: { id: project.id, title, description, subhead },
         error,
       })
-    } finally {
-      setIsLoading(false)
     }
+  }
+
+  // Debounce the onSave function to handle delayed saving after user input stops
+  const debouncedSave = useDebounce(onSave)
+
+  // Update the project title state and trigger debounced save
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value)
+    debouncedSave()
+  }
+
+  // Update the project subhead state and trigger debounced save
+  const handleSubheadChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSubhead(event.target.value)
+    debouncedSave()
+  }
+
+  // Update the project description state and trigger debounced save
+  const handleDescriptionChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setDescription(event.target.value)
+    debouncedSave()
   }
 
   return (
@@ -50,7 +60,7 @@ export const EditProjectForm = ({ project }: { project: Project }) => {
             label="Project Title"
             value={title}
             placeholder="Enter the project title, eg. 'Power of the punch'"
-            onChange={(event) => setTitle(event.target.value)}
+            onChange={handleTitleChange}
           />
         </Grid>
         <Grid item xs={12} md={7} lg={8}>
@@ -59,7 +69,7 @@ export const EditProjectForm = ({ project }: { project: Project }) => {
             label="Project Subhead"
             value={subhead}
             placeholder="Use a small sentence to describe the project, eg. 'Students will learn Newtons Laws while constructing a boxing glove'"
-            onChange={(event) => setSubhead(event.target.value)}
+            onChange={handleSubheadChange}
           />
         </Grid>
         <Grid item xs={12}>
@@ -68,15 +78,12 @@ export const EditProjectForm = ({ project }: { project: Project }) => {
             label="Project Descripton"
             value={description}
             placeholder="Describe the project in detail (suggested length: 300 words)"
-            onChange={(event) => setDescription(event.target.value)}
+            onChange={handleDescriptionChange}
             multiline
             rows={4}
           />
         </Grid>
       </Grid>
-      <LoadingButton loading={isLoading} onClick={onSave} variant="contained">
-        Update Project
-      </LoadingButton>
     </Paper>
   )
 }
